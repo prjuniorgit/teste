687748457664d91ec25227e8572e1cff341b28de
junior <jose.fg.junior@gmail.com>
2020-06-26 09:15:06 -0300

    primeira versao do j316 cadastro de usuario e login

---
+++ projetoj316/gestao_usuarios/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class GestaoUsuariosConfig(AppConfig):
+    name = 'gestao_usuarios'
---
+++ venv_j316/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/prjunior/dev/proj_j316/venv_j316/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
